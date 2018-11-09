b503e2303588862295ce39cd88785f2e585f2bda
Gernot Vormayr <gvormayr@gmail.com>
2018-11-10 00:24:54 +0100

    Make update non-quiet

--- .travis.install.sh
+++ .travis.install.sh
@@ -4,5 +4,5 @@ set -ex
 
 wget http://apt-stable.ntop.org/`lsb_release -r | cut -f2`/all/apt-ntop-stable.deb
 sudo dpkg -i apt-ntop-stable.deb
-sudo apt-get update -qq
+sudo apt-get update
 sudo apt-get install linux-headers-generic pfring-dkms pfring qemu-system-x86 libpcap-dev -y
